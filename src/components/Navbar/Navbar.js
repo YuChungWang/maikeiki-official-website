5e38411d6cc858fb615e6738a5dfad964726dcfc
charlie.wang <charliewang324@gmail.com>
2020-03-23 23:47:02 +0800

    feat: #Navbar -  default shape of navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import classnames from 'classnames';
 import styles from './Navbar.module.scss';
 
 class Navbar extends PureComponent {
@@ -6,7 +7,21 @@ class Navbar extends PureComponent {
     return (
       <>
         <div className={styles.container}>
-          <div className={styles.logo} />
+          <div className={styles['main-section']}>
+            <div className={styles['logo-section']}>
+              <div className={styles.logo} />
+            </div>
+            <div className={styles['route-text']}>關於Mai Keiki</div>
+            <div className={styles['route-text']}>所有產品</div>
+            <div className={styles['route-text']}>食材使用</div>
+            <div className={styles['route-text']}>訂購需知</div>
+          </div>
+          <div className={styles['third-part-link-section']}>
+            <div className={classnames(styles.icon, styles['icon-facebook'])} />
+            <div
+              className={classnames(styles.icon, styles['icon-instagram'])}
+            />
+          </div>
         </div>
       </>
     );
